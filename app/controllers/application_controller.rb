ef4c781a5d78d255221456e389b1a98ba9e45a79
VictoriaTodd <a1719597@student.adelaide.edu>
2019-08-27 13:22:44 +0930

    add microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def hello
-		render html: "<b> hello, world! </b>"
+		render html: "hello, world!"
 	end
 
 end
